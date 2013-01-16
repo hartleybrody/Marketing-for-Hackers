0815834bf2fcd80e17d6c225b0436c2afe805a7b
Hartley Brody <hbrody@hubspot.com>
2013-01-16 13:43:59 -0500

    cleaned up some janky python and added a lead dump page

--- landingpage/views.py
+++ landingpage/views.py
@@ -7,54 +7,66 @@ from marketingforhackers.landingpage.models import Lead
 from mailsnake import MailSnake
 from marketingforhackers import settings
 
+
 def index(request):
     # try to figure out where they came from
-    try:
-        referrer = request.META.get('HTTP_REFERER')
-    except AttributeError:
-        referrer = "direct"
-        
+    referrer = request.META.get('HTTP_REFERER')
     analytics = {"referrer": referrer}
-    
+
     return render_to_response("index.html", analytics, context_instance=RequestContext(request))
-    
+
+
 def submit(request):
     # grab info
     email = request.POST['email']
     referrer = request.POST['referrer']
-    
+
     # save locally
     lead_to_submit = Lead(email=email, referrer=referrer)
     lead_to_submit.save()
-    
+
     # send to Mailchimp
     key = settings.MAILCHIMP_API_KEY
-    list = settings.MAILCHIMP_LIST_NUM
-    
+    list_num = settings.MAILCHIMP_LIST_NUM
+
     # see: http://www.nerdydork.com/integrate-mailchimp-with-your-django-app.html
     mailsnake = MailSnake(key)
-    mailsnake.listSubscribe(
-        id = list,
-        email_address = email,
-        double_optin = False,
-        send_welcome = True
-    )
-    
-    return  HttpResponseRedirect(reverse('landingpage.views.thanks'))
-    
+    try:
+        mailsnake.listSubscribe(
+            id=list_num,
+            email_address=email,
+            double_optin=False,
+            send_welcome=True
+        )
+    except:
+        pass
+
+    return HttpResponseRedirect(reverse('landingpage.views.thanks'))
+
+
 def thanks(request):
     return render_to_response("thanks.html")
-    
+
+
 def author(request):
     import hashlib, urllib
-    
+
     #get gravatar URL, from: http://en.gravatar.com/site/implement/images/python/
     email = "hartley.brody@gmail.com"
     size = 150
     gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
-    gravatar_url += urllib.urlencode({'s':str(size)})
+    gravatar_url += urllib.urlencode({'s': str(size)})
 
     author_info = dict(thumbnail=gravatar_url)
-    
+
     return render_to_response("author.html", author_info)
-    
+
+
+def view_leads(request):
+    if not request.GET.get("pass") == settings.VIEW_LEADS_PASSWORD:
+        return HttpResponse("those are private", status=403)
+
+    all_leads = Lead.objects.all()
+    total = len(all_leads)
+
+    return render_to_response("dump.html", data=dict(leads=all_leads, total=total))
